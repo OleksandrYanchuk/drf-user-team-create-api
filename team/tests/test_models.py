a7abccfc5f43e28f15382b06c03264661ea4ef8a
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-10-19 14:38:56 +0300

    write some tests

--- team/tests/test_models.py
+++ team/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from user.models import User
+
 from team.models import Team, TeamMember
+from user.models import User
 
 
 class TeamMemberModelTest(TestCase):
@@ -10,3 +11,26 @@ class TeamMemberModelTest(TestCase):
         self.team_member = TeamMember.objects.create(
             user=self.user, status="Developer", team=self.team
         )
+
+    def test_team_member_creation(self):
+        team_member = TeamMember.objects.get(id=self.team_member.id)
+        self.assertEqual(team_member.user, self.user)
+        self.assertEqual(team_member.status, "Developer")
+        self.assertEqual(team_member.team, self.team)
+
+    def test_team_member_str_method(self):
+        expected_str = f"({self.team.name}){self.user.first_name} {self.user.last_name}"
+        self.assertEqual(str(self.team_member), expected_str)
+
+
+class TeamModelTest(TestCase):
+    def setUp(self):
+        self.team = Team.objects.create(name="Test Team", about="Test description")
+
+    def test_team_creation(self):
+        team = Team.objects.get(id=self.team.id)
+        self.assertEqual(team.name, "Test Team")
+        self.assertEqual(team.about, "Test description")
+
+    def test_team_str_method(self):
+        self.assertEqual(str(self.team), "Test Team")
